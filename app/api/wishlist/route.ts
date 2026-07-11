2a2eb7f9989efea9f88ca9beb5875c750db364a7
nishadinfo09 <mdnishadali03@gmail.com>
2026-07-11 20:27:37 +0600

    add wishlist POST endpoint

--- app/api/wishlist/route.ts
+++ app/api/wishlist/route.ts
@@ -1,47 +1,59 @@
 import { db } from '@/lib/db/db';
 import { wishlists } from '@/lib/db/schema';
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
+import { getServerSession } from 'next-auth';
 
-export async function GET(request: Request) {
-  const { bookId } = await request.json();
-  if (!bookId) {
-    return Response.json({ message: 'Book ID is required' }, { status: 400 });
-  }
-
+export async function POST(request: Request) {
   try {
-    //1. Check if the book is already in the wishlist
-    //2. If not, add the book to the wishlist
-    //3  If yes, remove the book from the wishlist
-    //4. Return the updated wishlist
-
-    const existingWishlist = await db
-      .select()
-      .from(wishlists)
-      .where(eq(wishlists.bookId, bookId))
-      .limit(1);
-
-    if (existingWishlist.length > 0) {
-      // Book is already in the wishlist, remove it
-      await db.delete(wishlists).where(eq(wishlists.bookId, bookId));
-      return Response.json(
-        { message: 'Book removed from wishlist' },
-        { status: 200 },
-      );
+    const session = await getServerSession();
+    if (!session?.user?.id) {
+      return Response.json({ message: 'Unauthorized' }, { status: 401 });
     }
 
-    // Book is not in the wishlist, add it
-    await db
-      .insert(wishlists)
-      .values({ bookId, userId: 'user-id-placeholder' });
+    const { bookId } = await request.json();
+    if (!bookId) {
+      return Response.json({ message: 'Book ID is required' }, { status: 400 });
+    }
 
-    return Response.json(
-      { message: 'Book added to wishlist' },
-      { status: 200 },
-    );
+    const userId = session.user.id;
+
+    const wishlistItem = await db
+      .select()
+      .from(wishlists)
+      .where(and(eq(wishlists.bookId, bookId), eq(wishlists.userId, userId)))
+      .limit(1);
+
+    if (wishlistItem) {
+      await db
+        .delete(wishlists)
+        .where(and(eq(wishlists.userId, userId), eq(wishlists.bookId, bookId)));
+
+      return Response.json({
+        success: true,
+        inWishlist: false,
+        message: 'Removed from wishlist',
+      });
+    }
+
+    await db.insert(wishlists).values({
+      userId,
+      bookId,
+    });
+
+    return Response.json({
+      success: true,
+      inWishlist: true,
+      message: 'Added to wishlist',
+    });
   } catch (error) {
     return Response.json(
-      { message: 'Error while getting wishlist items' },
-      { status: 500 },
+      {
+        success: false,
+        message: 'Internal Server Error',
+      },
+      {
+        status: 500,
+      },
     );
   }
 }
