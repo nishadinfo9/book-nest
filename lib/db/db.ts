79811ae3bf29a4ee369e7c5e9b683c411ea9ccb3
nishadinfo09 <mdnishadali03@gmail.com>
2026-07-24 06:37:01 +0600

    update datavase config

--- lib/db/db.ts
+++ lib/db/db.ts
@@ -1,7 +1,12 @@
+
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 const queryString = process.env.DATABASE_URL as string;
-export const connection = postgres(queryString);
+export const connection = postgres(queryString, {
+    max: 5,
+    idle_timeout: 20,
+    connect_timeout: 20
+});
 
 export const db = drizzle({ client: connection });
